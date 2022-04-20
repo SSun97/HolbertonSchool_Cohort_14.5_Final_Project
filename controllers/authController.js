c401263824380b57dd4a518595478d1eed19bc62
Ubuntu <ubuntu@ip-172-31-28-132.ec2.internal>
2022-04-20 00:52:35 +0000

    changed SMTP from mailtrap to AWS SES

--- controllers/authController.js
+++ controllers/authController.js
@@ -117,6 +117,7 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   const resetURL = `${req.protocol}://${req.get(
     'host'
   )}/api/v1/users/resetPassword/${resetToken}`;
+  console.log(resetURL);
   const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
   try {
     await sendEmail({
--- utils/email.js
+++ utils/email.js
@@ -3,18 +3,18 @@ const nodemailer = require('nodemailer');
 const sendEmail = async (options) => {
   // 1) Create a transporter
   const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
+    host: process.env.AWS_EMAIL_HOST,
+    port: process.env.AWS_EMAIL_PORT,
     auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
+      user: process.env.AWS_EMAIL_USERNAME,
+      pass: process.env.AWS_EMAIL_PASSWORD,
     },
   });
-  console.log(transporter);
+  // console.log(transporter);
   // Activate in gmail "less secure app"
   // 2) Define the email options
   const mailOptions = {
-    from: 'Simon Sun <sunmingyu@live.cn>',
+    from: 'admin NFTXRVR <2683@holbertonschool.com>',
     to: options.email,
     subject: options.subject,
     text: options.message,
