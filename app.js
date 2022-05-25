55d272536b57cc16d81ae63ac5def2f54213fa85
ssun97 <sunmingyu.97@gmail.com>
2022-05-24 22:58:51 -0500

    deactive view router

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/', viewRouter);
+// app.use('/', viewRouter);
 app.use('/api/v1/prods', prodRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
